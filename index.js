547fd9d0314f2d29c8341b1aaa328a41c5306649
tjtanaa <tunjian.tan@embeddedllm.com>
2024-05-31 21:51:36 +0800

    test example

--- index.js
+++ index.js
@@ -1,61 +1,76 @@
-const express = require("express");
-import JamAI from "jamaibase";
+// const express = require("express");
+// import JamAI from "jamaibase";
 
-const jamai = new JamAI({
-    baseURL: process.env["BASEURL"]!,
-    apiKey: process.env["JAMAI_APIKEY"]!,
-    projectId: process.env["PROJECT_ID"]!
-});
+// const jamai = new JamAI({
+//     baseURL: process.env["BASEURL"]!,
+//     apiKey: process.env["JAMAI_APIKEY"]!,
+//     projectId: process.env["PROJECT_ID"]!
+// });
 
-const app = express();
+// const app = express();
 
-app.get("/", (req: any, res: any) => res.send("Express on Vercel"));
+// app.get("/", (req: any, res: any) => res.send("Express on Vercel"));
 
 
-app.get("/list-tables", async (req: any, res: any) => {
-    const response = await jamai.addRow({
-        table_type: "action",
-        data: [
-            {
-                Country: "Germany"
-            },
-            {
-                Country: "USA"
-            },
-            {
-                Country: "Malaysia"
-            },
-            {
-                Country: "UK"
-            },
-            {
-                Country: "Chile"
-            },
-            {
-                Country: "Bhutan"
-            },
-            {
-                Country: "France"
-            }
-        ],
-        table_id: "CountryCapital",
-        reindex: true,
-        concurrent: true
-    });
+// app.get("/list-tables", async (req: any, res: any) => {
+//     const response = await jamai.addRow({
+//         table_type: "action",
+//         data: [
+//             {
+//                 Country: "Germany"
+//             },
+//             {
+//                 Country: "USA"
+//             },
+//             {
+//                 Country: "Malaysia"
+//             },
+//             {
+//                 Country: "UK"
+//             },
+//             {
+//                 Country: "Chile"
+//             },
+//             {
+//                 Country: "Bhutan"
+//             },
+//             {
+//                 Country: "France"
+//             }
+//         ],
+//         table_id: "CountryCapital",
+//         reindex: true,
+//         concurrent: true
+//     });
   
-    const listRowResponse = await jamai.listRows({
-        table_type: "action",
-        table_id: "CountryCapital"
-    });
-    res.send(JSON.stringify(listRowResponse));
+//     const listRowResponse = await jamai.listRows({
+//         table_type: "action",
+//         table_id: "CountryCapital"
+//     });
+//     res.send(JSON.stringify(listRowResponse));
 
-}
+// }
     
 
 
-);
+// );
 
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
+// app.listen(3000, () => console.log("Server ready on port 3000."));
 
+// module.exports = app;
+
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Express on Vercel");
+});
+
+app.listen(5000, () => {
+  console.log("Running on port 5000.");
+});
+
+// Export the Express API
 module.exports = app;
